3eacfd96f0a21516b6f14afa328712047e4cc7e4
Matt Johnson <matt.t.johnson116@gmail.com>
2017-04-22 14:32:40 -0400

    added YT search call

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Keys from '../Keys';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = '';
+const API_KEY = Keys.youtube;
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
 
 const App = function() {
 	return (
