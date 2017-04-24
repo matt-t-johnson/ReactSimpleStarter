5220652cc825064ec19bbef1525e2fbdef4213d1
Matthew T Johnson <matthew.t.johnson@av-mbp-7209.attlocal.net>
2017-04-24 19:12:46 -0400

    added to video list components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Keys from '../Keys';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = Keys.youtube;
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = function() {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+    this.state = {
+      videos: [],
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    	this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+  		<div>
+  			<SearchBar />
+        <VideoList videos={this.state.videos}/>
+  		</div>
+  	);
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
