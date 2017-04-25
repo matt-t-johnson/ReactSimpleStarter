ed94cb54224e8620a3033eac3abc7447c2fc3579
Matthew T Johnson <matthew.t.johnson@av-mbp-7209.attlocal.net>
2017-04-24 21:23:06 -0400

    added video detail component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = Keys.youtube;
 
@@ -24,6 +25,7 @@ class App extends Component {
     return (
   		<div>
   			<SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
   		</div>
   	);
